09e5b2277103dc82ba3ee719dfe14f4e68545805
Chamath Goonetilleke <31180821+Chamath-Goonetilleke@users.noreply.github.com>
2025-05-03 09:41:16 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,12 +2,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AdminDashboard from './page/AdminDashboard';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      <SpeedInsights/>
       <Routes>
         <Route path="/" element={<AdminDashboard />} />
       </Routes>
